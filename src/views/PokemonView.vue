cae7cd24eb2f5beb3adeea269caceb45c6c52502
Mauran <thomas.mauran@etu.umontpellier.fr>
2022-11-13 19:25:45 +0100

    Adding reccursive function to retrieve the evolutions

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -3,7 +3,8 @@ import P from "../services/Pokedex"
 import { useRoute } from 'vue-router';
 import { onMounted, ref } from "vue"
 import pokemonRadarChart from "../components/pokemonRadarChart.vue";
-import {randomBorderRadius, randomColorBg} from "../services/randomStyle"
+import { randomBorderRadius, randomColorBg } from "../services/randomStyle"
+import pokemonCard from "../components/pokemonCard.vue";
 
 const route = useRoute();
 const name = route.params.id;
@@ -28,23 +29,42 @@ function fetchPokemonInfo() {
         response.stats.forEach(stat => {
             statsList.value.push(stat.base_stat)
         })
+        fetchEvolutionChain(pokemon.value.id)
 
-        
     })
 
 
 }
 
-function fetchEvolutionChain(id){
-    P.getEvolutionChainById(id).then(function(response){
-        evolutionChain.value = response
-        console.log(evolutionChain.value)
+function fetchEvolutionChain(id) {
+    evolutionChain.value += {
+        name: pokemon.value.name,
+        img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}` 
+    }
+    P.getEvolutionChainById(id).then(function (response) {
+
+        reccursiveEvolRetrieve(response.chain)
+        // evolutionChain.value += {
+        //     name: response.chain.evolves_to[0].species.name
+        //     img: 
+        // }
     })
+}
+
+function reccursiveEvolRetrieve(arr){
+    // Base case
+
+    console.log(arr)
+    console.log(arr.hasOwnProperty('evolves_to'))
+    if(!arr.evolves_to){
+        return
+    }
+    console.log(arr.species.name)
+    reccursiveEvolRetrieve(arr.evolves_to[0])
 }
 
 onMounted(() => {
     fetchPokemonInfo()
-    // fetchEvolutionChain(pokemon.value.id)
 })
 
 </script>
@@ -55,65 +75,71 @@ onMounted(() => {
         <div class="container">
             <article>
                 <img class="pokeImgBig" :src="pokeImg" alt="image of the {{pokemon.name}}" :style="customStyle">
+                <pokemonRadarChart class="chart" :seriesData="statsList" />
 
             </article>
 
             <article>
                 <h1 class="bigTitle">{{ pokemon.name }}</h1>
                 <div class="typeDiv">
-                    <h3 v-for="t in pokeType" :class="t.type.name">{{t.type.name}}</h3>
+                    <h3 v-for="t in pokeType" :class="t.type.name">{{ t.type.name }}</h3>
                 </div>
+
                 <ul>
+                    <li>Id : #{{pokemon.id}}</li>
                     <li>Height : {{ pokemon.height }}</li>
                     <li>Weight : {{ pokemon.weight }}</li>
-                    <li>Base experience : {{pokemon.base_experience}}</li>
+                    <li>Base experience : {{ pokemon.base_experience }}</li>
                 </ul>
 
+                <h1>Evolutions :</h1>
+                
 
-                <pokemonRadarChart class="chart" :seriesData="statsList" />
 
 
             </article>
+
         </div>
     </div>
 
 </template>
 <style scoped>
-
-
 /* Actual css */
 
-.chart{
+.chart {
     padding-right: 100px;
 }
-.bigTitle{
+
+.bigTitle {
     font-size: 3em;
 }
-.typeDiv{
+
+.typeDiv {
     display: flex;
 }
 
-.typeDiv h3{
+.typeDiv h3 {
     border-radius: 10px;
     padding: 5px;
     color: white;
     margin: 10px;
 }
 
-.pokeImgBig{
+.pokeImgBig {
     width: 20vw;
     padding: 20px;
     min-width: 200px;
 }
+
 .mainContentDiv {
     display: flex;
     flex-direction: row;
     justify-content: center;
-    
+
     margin: 20px;
 }
 
-.container{
+.container {
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -132,69 +158,71 @@ article {
 
 /* Type colors  */
 
-.normal{
+.normal {
     background-color: grey;
 }
 
-.flying{
+.flying {
     background-color: rgb(130, 207, 207);
 }
-.bug{
+
+.bug {
     background-color: rgb(155, 184, 111);
 }
 
-.ground{
+.ground {
     background-color: rgb(188, 157, 100);
 }
 
-.rock{
+.rock {
     background-color: rgb(85, 53, 8);
 }
-.grass{
+
+.grass {
     background-color: rgb(40, 140, 40);
 }
 
-.ghost{
+.ghost {
     background-color: rgb(86, 86, 167);
 }
-.dark{
+
+.dark {
     background-color: rgb(27, 21, 18);
 }
 
-.electric{
+.electric {
     background-color: rgb(236, 211, 48);
 }
 
-.psychic{
+.psychic {
     background-color: purple;
 }
 
-.steel{
+.steel {
     background-color: rgb(147, 161, 161);
 }
 
-.fighting{
+.fighting {
     background-color: rgb(193, 92, 56);
 }
 
-.ice{
+.ice {
     background-color: rgb(117, 209, 193);
 }
 
-.fairy{
+.fairy {
     background-color: rgb(232, 183, 191);
 }
 
-.poison{
+.poison {
     background-color: blueviolet;
 }
 
-.fire{
+.fire {
     background-color: orange;
 }
 
-.water{
+.water {
     background-color: blue;
 }
-
 </style>
